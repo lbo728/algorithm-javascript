4bec0955963b4cdafe042af844bbf390da99d805
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-03-27 22:40:48 +0900

    unsolve: 01-09

--- 01/08_일곱난쟁이.js
+++ 01/08_일곱난쟁이.js
@@ -1,6 +1,15 @@
 function solution(arr) {
   let answer = arr;
 
+  // 1. arr 배열의 합이 100 미만인 경우만 반환하는 배열을 생성
+  // 조건, 총합이 100을 넘지 않는 7개 수
+
+  let sum;
+  for (let i = 0; i < arr.length; i++) {
+    if (sum < 100) {
+      sum = sum + arr[i];
+    }
+  }
   return answer;
 }
 
